48df1471aa9ec63d7988f03554601a709b56f8df
rajbot <raj@archive.org>
2012-01-13 23:34:22 +0000

    look up keys in dict, not keys in string

--- fac.py
+++ fac.py
@@ -9,15 +9,22 @@ def facet_dict(string):
     for facet in facet_list:
         k,v = facet.split(',')[0], facet.split(',')[-1]
         dictionary[k] = v
+    print len(dictionary)
     faceted = {}
-    for k,v in dictionary.iteritems():
-        if k in string:
-            faceted[k] = v
+    words = string.split()
+    for word in words:
+        print word
+        if word in dictionary:
+            faceted[word] = dictionary[word]
+
+    #for k,v in dictionary.iteritems():
+    #    if k in string:
+    #        faceted[k] = v
     return faceted
 
 def main():
     #t = open('text','rb').read()
-    print facet_dict(' STS-135 ')
+    print facet_dict(' STS-1 STS-123 ')
 
 if __name__ == "__main__":
     main()
